9d88a2a543bcdfa64426869f73ee5dc4377fcbf1
王纯 <wangchun@wangchundeMacBook-Pro.local>
2020-05-15 22:07:55 +0800

    modify

--- myapp/src/pages/relationship/relationship.js
+++ myapp/src/pages/relationship/relationship.js
@@ -3,7 +3,7 @@ export default class Relationship extends React.Component{
     render(){
         return(
             <div>
-                2
+                3
             </div>
         )
     }
